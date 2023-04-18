8f4e6b839afe8481b6fceef69dd8e60f9f541562
1xd-zsy <1207010875@qq.com>
2023-04-18 11:39:53 +0800

    first commit

---
+++ main/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"taskpool/pool"
+)
+
+/*
+var (
+
+	foodOk   = false
+	foodName = ""
+	l        = &sync.Mutex{}
+	cond     = sync.NewCond(l)
+
+)
+
+	func makeFood() {
+		fmt.Print("start make food\n")
+		time.Sleep(3 * time.Second)
+		foodOk = true
+		foodName = "a"
+		fmt.Print("food ok\n")
+		cond.Broadcast()
+	}
+
+	func waitToEat() {
+		cond.L.Lock()
+		defer cond.L.Unlock()
+		for !foodOk {
+			cond.Wait()
+		}
+		fmt.Printf("eat food :%s\n", foodName)
+	}
+*/
+func task(i int) {
+	fmt.Printf("task :%d\n", i)
+	time.Sleep(1 * time.Second)
+}
+func main() {
+	fmt.Println("vim-go")
+	taskPool, err := pool.BuildPool(pool.Size(5),
+		pool.MaxWaitTaskNum(10), pool.ExpreWorkerCleanInterval(10),
+		pool.PanicHandler(func(i interface{}) {
+			fmt.Printf("execute error:%v", i)
+		}))
+
+	if err != nil {
+		fmt.Printf("init error:%v", err)
+		panic(err)
+	}
+	go func() {
+		for {
+			fmt.Println("waiting", taskPool.Waiting())
+			time.Sleep(10 * time.Second)
+		}
+
+	}()
+	for i := 0; i < 100; i++ {
+		a := i
+		fn := func() {
+			time.Sleep(3 * time.Second)
+			fmt.Println("run task :%d\n", a)
+		}
+		go func() {
+			if err := taskPool.Submit(fn); err != nil {
+				fmt.Println(i, err)
+			}
+		}()
+
+	}
+	//time.Sleep(1 * time.Second)
+	//taskPool.Exit()
+
+}
---
+++ pool/pool.go
@@ -0,0 +1,198 @@
+package pool
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"sync"
+	"time"
+
+	"sync/atomic"
+)
+
+const closeState int32 = int32(1)
+
+type Pool struct {
+	running int32
+	lock    sync.Locker
+	workers *workArray
+	state   int32
+	cond    *sync.Cond
+	waiting int32
+
+	stopHeartbeat context.CancelFunc
+	ctx           context.Context
+	options       *Options
+}
+
+func BuildPool(options ...Option) (*Pool, error) {
+	opts := loadOptions(options...)
+	if opts.Size <= 0 {
+		return nil, fmt.Errorf("size less than 0")
+	}
+
+	pool := &Pool{
+		lock:    &sync.Mutex{},
+		options: opts,
+	}
+
+	pool.workers = newWorkArray(int(opts.Size))
+	pool.cond = sync.NewCond(pool.lock)
+
+	var ctx context.Context
+	ctx, pool.stopHeartbeat = context.WithCancel(context.Background())
+	if pool.options.ExpireWorkerCleanInterval != 0 {
+		go pool.delExpiredWorker(ctx)
+	}
+	return pool, nil
+}
+
+//func (p *Pool) Exit() error {
+//	p.setClose()
+//	fmt.Println("exiting")
+//	for p.Waiting() > 0 {
+//
+//	}
+//	p.stopHeartbeat()
+//	p.workers.exit()
+//	fmt.Println("exited")
+//	return nil
+//}
+//
+//func (p *Pool) isExit() bool {
+//	if atomic.LoadInt32(&p.state) == closeState {
+//		return true
+//	}
+//	return false
+//}
+//
+//func (p *Pool) setClose() {
+//	atomic.StoreInt32(&p.state, closeState)
+//}
+
+func (p *Pool) Submit(task func()) error {
+	var w *poolWorker
+	//if p.isExit() {
+	//	return errors.New("pool exiting")
+	//}
+	if w = p.getWorker(); w == nil {
+		return errors.New("pool full")
+	}
+	w.task <- task
+	return nil
+}
+
+func (p *Pool) Cap() int {
+	return int(atomic.LoadInt32(&p.options.Size))
+}
+
+func (p *Pool) Running() int {
+	return int(atomic.LoadInt32(&p.running))
+}
+
+func (p *Pool) Waiting() int {
+	return int(atomic.LoadInt32(&p.waiting))
+}
+
+func (p *Pool) delExpiredWorker(ctx context.Context) {
+	hb := time.NewTimer(p.options.ExpireWorkerCleanInterval)
+
+	defer func() {
+		hb.Stop()
+	}()
+
+	for {
+		select {
+		case <-hb.C:
+		case <-ctx.Done():
+			return
+		}
+		p.lock.Lock()
+		expairedWorkers := p.workers.getExpiredWorker(p.options.ExpireWorkerCleanInterval)
+		p.lock.Unlock()
+
+		for i := range expairedWorkers {
+			expairedWorkers[i].task <- nil
+			expairedWorkers[i] = nil
+		}
+		// 有可能所有都过期了
+		if p.Running() > 0 || p.Waiting() > 0 {
+			p.cond.Broadcast()
+		}
+	}
+}
+
+func (p *Pool) putWorker(worker *poolWorker) bool {
+	if c := p.Cap(); p.Running() > c {
+		p.cond.Broadcast()
+		return false
+	}
+	worker.lastUseTime = time.Now()
+	p.lock.Lock()
+
+	p.workers.putWorker(worker)
+	// 这里只需要通知一个
+	p.cond.Signal()
+	p.lock.Unlock()
+	return true
+}
+
+func (p *Pool) getWorker() (w *poolWorker) {
+	newWorkerAndRun := func() {
+		w = &poolWorker{
+			pool: p,
+			task: make(chan func()),
+		}
+		w.execute(p.ctx)
+	}
+	p.lock.Lock()
+	// 有情况不能defer
+	// defer p.lock.Unlock()
+	w = p.workers.getWorker()
+	if w != nil {
+		p.lock.Unlock()
+		return
+	} else if c := p.Cap(); c > p.Running() {
+		p.lock.Unlock()
+		newWorkerAndRun()
+	} else {
+		// 这里需要无限循环
+		for {
+			if p.Waiting() >= int(p.options.MaxWaitTaskNum) {
+				p.lock.Unlock()
+				return
+			}
+			p.addWaiting(1)
+			p.cond.Wait()
+			p.addWaiting(-1)
+
+			var newWorkerNum int
+			if newWorkerNum = p.Running(); newWorkerNum == 0 {
+				p.lock.Unlock()
+				newWorkerAndRun()
+				return
+			}
+			if w = p.workers.getWorker(); w == nil {
+				if newWorkerNum < p.Cap() {
+					p.lock.Unlock()
+					newWorkerAndRun()
+					return
+				} else {
+					continue
+				}
+			} else {
+				break
+			}
+		}
+		p.lock.Unlock()
+	}
+	return
+}
+
+func (p *Pool) addRunning(t int) {
+	atomic.AddInt32(&p.running, int32(t))
+}
+
+func (p *Pool) addWaiting(t int) {
+	atomic.AddInt32(&p.waiting, int32(t))
+}
